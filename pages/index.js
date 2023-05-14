9cc39a9bd5b62b31a626348db71b9620526bc5ce
stormyy00 <jonathan.trujillo0432@gmail.com>
2023-05-14 16:55:15 -0700

    fixed mian page header

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,13 @@ const Layout = ({ children, title }) => {
         <title>{title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
+      <div>
       <header className={styles.navbar}>
         <nav>
           <ul>
             <li>
               <a href="/">Home</a>
+
             </li>
             <li>
               <a href="/about">About</a>
@@ -22,6 +24,12 @@ const Layout = ({ children, title }) => {
           </ul>
         </nav>
       </header>
+      <div className={styles.title}>
+      <h1>Welcome to my website!</h1>
+      </div>
+      <p className={styles.description}>This is the homepage.</p>
+
+      </div>
       <main>{children}</main>
     </div>
   )
