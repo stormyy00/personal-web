307193d842de6aec1c972b2d2e9c10356bdfe61c
stormyy00 <jonathan.trujillo0432@gmail.com>
2023-05-14 00:09:10 -0700

    functioning basics

--- pages/home.js
+++ pages/home.js
@@ -3,10 +3,10 @@ import styles from '@/styles/Home.module.css'
 const Home = () => {
   return (
     <Layout title="Home">
-      <div className={`${styles.description} ${styles.title}`}>
+      <div className={styles.title}>
       <h1>Welcome to my website!</h1>
       </div>
-      <p>This is the homepage.</p>
+      <p className={styles.description}>This is the homepage.</p>
       <div>
 
 lol
